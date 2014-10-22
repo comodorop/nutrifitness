7203a5cb2d1551c8ba43c15c1896c7dbbb3e2a98
torres <torres@torres-Lenovo-G460>
2014-10-21 21:11:31 -0500

    daoConneciton

---
+++ NutriFitness/administracion/menu.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+    <!-- Mirrored from responsivewebinc.com/premium/cakefactory12/0-base.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 17 Oct 2014 04:11:59 GMT -->
+    <head>
+        <meta charset="utf-8">
+        <!-- Title here -->
+        <title>Your Title - CakeFactory</title>
+        <!-- Description, Keywords and Author -->
+        <meta name="description" content="Your description">
+        <meta name="keywords" content="Your,Keywords">
+        <meta name="author" content="ResponsiveWebInc">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Styles -->
+        <!-- Bootstrap CSS -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <!-- Portfolio CSS -->
+        <link href="../css/prettyPhoto.css" rel="stylesheet">
+        <!-- Font awesome CSS -->
+        <link href="../css/font-awesome.min.css" rel="stylesheet">	
+        <!-- Custom Less -->
+        <link href="../css/less-style.css" rel="stylesheet">	
+        <!-- Custom CSS -->
+        <link href="../css/style.css" rel="stylesheet">
+        <!--[if IE]><link rel="stylesheet" href="css/ie-style.css"><![endif]-->
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="#">
+    </head>
+
+    <body>
+
+
+
+        <!-- Page Wrapper -->
+        <div class="wrapper">
+            <!-- Header Start -->
+            <div class="header">
+            </div>
+            <!-- Header End -->
+            <!-- Banner Start -->
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">NutriFitness</a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#" id="slider">Slider</a></li>
+                            <li><a href="#">Link</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+            <div class="inner-page padd">
+                <div class="showcase">
+                    <div class="container">
+                        <div class="row">
+                            <div class="checkout">
+                                <div class="container" id="contenedor">
+                                    <!-- Heading -->
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Showcase End -->
+            </div><!-- / Inner Page Content End -->	
+
+            <!-- Footer Start -->
+
+
+
+            <!-- Footer End -->
+
+        </div><!-- / Wrapper End -->
+
+
+        <!-- Scroll to top -->
+        <span class="totop"><a href="#"><i class="fa fa-angle-up"></i></a></span> 
+        <!-- Javascript files -->
+        <!-- jQuery -->
+        <script src="../js/jquery.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="../js/bootstrap.min.js"></script>
+        <!-- Pretty Photo JS -->
+        <script src="../js/jquery.prettyPhoto.js"></script>
+        <!-- Respond JS for IE8 -->
+        <script src="../js/respond.min.js"></script>
+        <!-- HTML5 Support for IE -->
+        <script src="../js/html5shiv.js"></script>
+        <!-- Custom JS -->
+        <script src="../js/custom.js"></script>
+        <script src="js/Slider.js"></script>
+        <!-- JS code for this page -->
+        <script>
+
+        </script>
+    </body>	
+
+    <!-- Mirrored from responsivewebinc.com/premium/cakefactory12/0-base.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 17 Oct 2014 04:11:59 GMT -->
+</html>
