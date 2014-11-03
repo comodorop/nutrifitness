83697e5fbe8e8dfd23d20a6a8c3b6bad762fee54
Joel <Joel@JACA-PC>
2014-11-02 21:59:30 -0600

    De php a Angular con la version 1.3.X
    Por fin pude subir cambios.

--- NutriFitness/dameNombres.php
+++ NutriFitness/dameNombres.php
@@ -1,29 +1,24 @@
 <?php
-
 include './daoconexion/daoConeccion.php';
 $cn = new coneccion();
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
 $cn->Conectarse();
 $sql = "SELECT * FROM slider";
 $result = mysql_query($sql);
-
-//$array;
-//
-//while ($rs = mysql_fetch_array($result)) {
-//    $array = array('nombre' => $rs["imagen"]);
-//}
-
-
-$outp = "[";
+$arr = array();
+$contador = 0;
 while ($rs = mysql_fetch_array($result)) {
-    if ($outp != "[") {
-        $outp .= ",";
-    }
-    $outp .= '{"Name":"' . $rs["imagen"] . '"}';
+    $arr['data'][$contador] = array('img' => $rs["imagen"]);
+    $contador++;
 }
-$outp .="]";
-echo($outp);
+echo "imagenes: " . json_encode($arr['data']);
+//==============================================================================
+//$outp = "[";
+//while ($rs = mysql_fetch_array($result)) {
+//    if ($outp != "[") {
+//        $outp .= ",";
+//    }
+//    $outp .= '{"name":"' . $rs["imagen"] . '"}';
+//}
+//$outp .="]";
+//echo $outp;
 //echo json_encode($array);
